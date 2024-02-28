ad58f13abd3c7d3c906a74887f4ed4b7f02b2c94
Oseias-Isidoro <oseiasisidoro@gmail.br>
2024-02-28 02:00:47 -0300

    create MessageBuilder.php

---
+++ src/Messages/MessageBuilder.php
@@ -0,0 +1,110 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EvolutionSDK\Messages;
+
+use EvolutionSDK\Interfaces\MessageBuilderInterface;
+
+class MessageBuilder implements MessageBuilderInterface
+{
+    private array $message;
+    private string $instance;
+
+    public function __construct()
+    {
+        $this->message['options'] = [
+            "delay" => 1200,
+            "presence" => "composing"
+        ];
+    }
+
+    public function from(string $instance): MessageBuilder
+    {
+        $this->instance = $instance;
+
+        return $this;
+    }
+
+    public function to(string $number): MessageBuilder
+    {
+        $this->message['number'] = $number;
+
+        return $this;
+    }
+
+    public function text(string $text): MessageBuilder
+    {
+        $this->message['textMessage'] = [
+            'text' => $text
+        ];
+
+        return $this;
+    }
+
+    public function media(string $url, string $mediaType, string $fileName = null): MessageBuilder
+    {
+        $this->message['mediaMessage'] = [
+            'mediatype' => $mediaType,
+            'media' => $url,
+        ];
+
+        if ($fileName and $mediaType != 'image') {
+            $this->message['mediaMessage']['fileName'] = $fileName;
+        }
+
+        return $this;
+    }
+
+    public function audio(string $url): MessageBuilder
+    {
+        $this->message['options']['presence'] = 'recording';
+        $this->message['options']['encoding'] = true;
+
+        $this->message['audioMessage'] = [
+            'audio' => $url,
+        ];
+
+        return $this;
+    }
+
+    public function mentions(bool $everyOne = true, array $data = []): MessageBuilder
+    {
+        $this->message['mentions'] = [
+            "everyOne" => $everyOne,
+        ];
+
+        if (!$everyOne) {
+            $this->message['mentions']['mentioned'] = $data;
+        }
+
+        return $this;
+    }
+
+    public function reply(array $data): MessageBuilder
+    {
+        $this->message['options']['quoted'] = $data;
+
+        return $this;
+    }
+
+    private function hasMedia(): bool
+    {
+        return isset($this->message['mediaMessage']);
+    }
+
+    private function hasText(): bool
+    {
+        return isset($this->message['textMessage']);
+    }
+
+    public function getMessage(): Message
+    {
+        if ($this->hasMedia() and $this->hasText()) {
+            $this->message['mediaMessage']['caption'] = $this->message['textMessage']['text'];
+            unset($this->message['textMessage']);
+        }
+
+        return new Message($this->message, $this->instance);
+    }
+}
