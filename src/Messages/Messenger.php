6c736ca07307dc6bc07ecb196e90cc234725e803
Oseias-Isidoro <oseiasisidoro@gmail.br>
2024-02-28 02:02:19 -0300

    create Messenger.php

---
+++ src/Messages/Messenger.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EvolutionSDK\Messages;
+
+use EvolutionSDK\HttpClient\API;
+use GuzzleHttp\Exception\GuzzleException;
+
+class Messenger
+{
+    private API $API;
+
+    public function __construct()
+    {
+        $this->API = new API();
+    }
+
+    /**
+     * @throws GuzzleException
+     */
+    public function send(Message $message): bool
+    {
+        $response = $this->API->post(
+            $this->getUri($message),
+            $message->getData()
+        );
+
+        return $response->successful();
+    }
+
+    private function getUri(Message $message): string
+    {
+        if ($message->hasMedia()) {
+            return $this->uriGenerator('sendMedia', $message->getInstance());
+        }
+
+        if ($message->hasAudio()) {
+            return $this->uriGenerator('sendWhatsAppAudio', $message->getInstance());
+        }
+
+        return $this->uriGenerator('sendText', $message->getInstance());
+    }
+
+    private function uriGenerator(string $typeMessage, string $instance): string
+    {
+        return '/message/'. $typeMessage .'/' . $instance;
+    }
+}
