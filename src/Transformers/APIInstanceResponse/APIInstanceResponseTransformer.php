fd5e91e09cd345bb9ed00690bd5eeee01b431aa9
Oseias-Isidoro <oseiasisidoro@gmail.br>
2024-03-13 11:33:21 -0300

    creates APIInstanceResponseTransformer.php

---
+++ src/Transformers/APIInstanceResponse/APIInstanceResponseTransformer.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace EvolutionSDK\Transformers\APIInstanceResponse;
+
+use EvolutionSDK\HttpClient\Response;
+use EvolutionSDK\Instances\Instance;
+
+class APIInstanceResponseTransformer
+{
+    private Response $response;
+
+    public function __construct(Response $response)
+    {
+        $this->response = $response;
+    }
+
+    public function toInstanceObject(): Instance
+    {
+        $instance = new Instance();
+
+        $response = $this->response->object();
+
+        $instance->setName($response->instance->instanceName);
+        $instance->setApikey($response->hash->apikey);
+
+        if (isset($response->qrcode)) {
+            $instance->setQrcode((array)$response->qrcode);
+        }
+
+        return  $instance;
+    }
+}
