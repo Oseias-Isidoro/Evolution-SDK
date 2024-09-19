cd718aa418857d1d55ef0493ebd5553892f2bcb1
oseias <oseiasisidoro@gmail.com>
2024-09-19 15:29:23 -0300

    update Message.php

--- src/Messages/Message.php
+++ src/Messages/Message.php
@@ -27,27 +27,27 @@ class Message
 
     public function hasContact(): bool
     {
-        return isset($this->data['contactMessage']);
+        return isset($this->data['contactMessage']) OR isset($this->data['contact']);
     }
 
     public function hasSticker(): bool
     {
-        return isset($this->data['stickerMessage']);
+        return isset($this->data['stickerMessage']) OR isset($this->data['sticker']);
     }
 
     public function hasLocation(): bool
     {
-        return isset($this->data['locationMessage']);
+        return isset($this->data['locationMessage']) OR (isset($this->data['latitude']) AND isset($this->data['longitude']));
     }
 
     public function hasMedia(): bool
     {
-        return isset($this->data['mediaMessage']);
+        return isset($this->data['mediaMessage']) OR isset($this->data['media']);
     }
 
     public function hasAudio(): bool
     {
-        return isset($this->data['audioMessage']);
+        return isset($this->data['audioMessage']) OR isset($this->data['audio']);
     }
 
     public function hasTemplate(): bool
