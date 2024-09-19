6ba5f5a21392f207c915661e5ee93f8c95798717
oseias <oseiasisidoro@gmail.com>
2024-09-19 14:56:09 -0300

    add return type for MessageBuilderInterface.php

--- src/Interfaces/MessageBuilderInterface.php
+++ src/Interfaces/MessageBuilderInterface.php
@@ -4,17 +4,17 @@ namespace EvolutionSDK\Interfaces;
 
 interface MessageBuilderInterface
 {
-    public function from(string $instance);
-    public function to(string $number);
-    public function text(string $text);
-    public function media(string $url, string $mediaType, string $fileName = null);
-    public function audio(string $url);
-    public function mentions(bool $everyOne = true, array $data = []);
-    public function reply(array $data);
-    public function location(string $name, string $latitude, string $longitude);
-    public function sticker(string $url);
-    public function contact(string $fullName, string $phoneNumber);
-    public function templateMessage(string $name, string $language, array $components);
+    public function from(string $instance): MessageBuilderInterface;
+    public function to(string $number): MessageBuilderInterface;
+    public function text(string $text): MessageBuilderInterface;
+    public function media(string $url, string $mediaType, string $fileName = null): MessageBuilderInterface;
+    public function audio(string $url): MessageBuilderInterface;
+    public function mentions(bool $everyOne = true, array $data = []): MessageBuilderInterface;
+    public function reply(array $data): MessageBuilderInterface;
+    public function location(string $name, string $latitude, string $longitude): MessageBuilderInterface;
+    public function sticker(string $url): MessageBuilderInterface;
+    public function contact(string $fullName, string $phoneNumber): MessageBuilderInterface;
+    public function templateMessage(string $name, string $language, array $components): MessageBuilderInterface;
     public function customField(string $key, string $value);
     public function getMessage();
 }
