47658b9d377e19d35a5ae26539933e507bcb7a4a
oseias <oseiasisidoro@gmail.com>
2024-09-21 10:48:03 -0300

    fix MessageBuilder.php

--- src/Messages/v2/MessageBuilder.php
+++ src/Messages/v2/MessageBuilder.php
@@ -118,19 +118,29 @@ class MessageBuilder implements MessageBuilderInterface
         return $this;
     }
 
+    public function hasContact(): bool
+    {
+        return isset($this->data['contactMessage']) OR isset($this->data['contact']);
+    }
+
+    public function hasSticker(): bool
+    {
+        return isset($this->data['stickerMessage']) OR isset($this->data['sticker']);
+    }
+
     public function hasLocation(): bool
     {
-        return isset($this->message['locationMessage']);
+        return isset($this->data['locationMessage']) OR (isset($this->data['latitude']) AND isset($this->data['longitude']));
     }
 
-    private function hasMedia(): bool
+    public function hasMedia(): bool
     {
-        return isset($this->message['mediaMessage']);
+        return isset($this->data['mediaMessage']) OR isset($this->data['media']);
     }
 
-    private function hasText(): bool
+    public function hasAudio(): bool
     {
-        return isset($this->message['text']);
+        return isset($this->data['audioMessage']) OR isset($this->data['audio']);
     }
 
     public function getMessage(): Message
